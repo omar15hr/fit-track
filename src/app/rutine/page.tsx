db7d4374a8c7b16e83b94866a3bfa1a23d6920f5
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-05-14 09:27:26 -0400

    rutine form created

--- src/app/rutine/page.tsx
+++ src/app/rutine/page.tsx
@@ -3,9 +3,17 @@ import { ExerciseCard } from "@/components/exercise-card";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 import { useRutineStore } from "@/app/store/store";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { RutineForm } from "@/components/rutine-form";
 
 export default function Rutine() {
-
   const rutine = useRutineStore((state) => state.rutine);
 
   const handleEdit = () => {
@@ -19,10 +27,23 @@ export default function Rutine() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-6">Tu Rutina de Entrenamiento</h1>
-      <Button className="mb-4 cursor-pointer">
-        <Plus className="h-4 w-4" />
-        Crear Rutina
-      </Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="mb-4 cursor-pointer">
+            <Plus className="h-4 w-4" />
+            Crear Rutina
+          </Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Crear Rutina</DialogTitle>
+            <DialogDescription>
+              Completa el formulario para crear una nueva rutina de entrenamiento
+            </DialogDescription>
+          </DialogHeader>
+          <RutineForm />
+        </DialogContent>
+      </Dialog>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {rutine.map((workout, index) => (
           <ExerciseCard
